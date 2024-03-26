ca0f57651d923db45490d73ea02ffee9bc706490
Reli <naitvenefy@gmail.com>
2024-03-26 13:55:45 -0600

    user can create posts

---
+++ src/app/components/compose-post.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Avatar } from '@nextui-org/react'
+import { ComposePostButton } from '@/app/components/compose-post-button'
+import { useRef } from 'react'
+import { addPost } from '@/app/actions/add-post-action'
+
+export function ComposePost ({ avatarUrl }: { avatarUrl: string }) {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  return (
+    <form
+      ref={formRef}
+      action={async (formData) => {
+        await addPost(formData)
+        formRef.current?.reset()
+      }}
+      className="flex flex-row gap-x-3 p-3 border-b border-white/20"
+    >
+      <Avatar radius="full" size="md" src={avatarUrl} />
+      <div className="flex flex-1 flex-col gap-y-4">
+        <textarea
+          name="content"
+          rows={4}
+          className="w-full text-xl bg-black placeholder-gray-500 p-2"
+          placeholder="What's on your mind?"
+        ></textarea>
+        <ComposePostButton />
+      </div>
+    </form>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { AuthButtonServer } from '@/app/components/auth-button-server'
 import PostsList from '@/app/components/posts-list'
 import { type Database } from '@/app/types/database'
 import { type Post } from '@/app/types/posts'
+import { ComposePost } from './components/compose-post'
 
 export default async function Home () {
   const supabase = createServerComponentClient<Database>({ cookies })
@@ -19,13 +20,15 @@ export default async function Home () {
   const { data: posts } = await supabase
     .from('posts')
     .select('*, user:users(name, user_name, avatar_url)')
+    .order('created_at', { ascending: false })
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <section className="max-w-[800px] mx-auto border-l border-r border-white/20 min-h-screen">
-        <AuthButtonServer />
+      <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
+        <ComposePost avatarUrl={session.user?.user_metadata.avatar_url} />
         <PostsList posts={posts as Post[]} />
       </section>
+      <AuthButtonServer />
     </main>
   )
 }
