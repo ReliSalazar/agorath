2ab89b6e01e898691d321d3ac5b14187e069c6de
Reli <naitvenefy@gmail.com>
2024-03-26 14:15:05 -0600

    what's happeninnnngggggg

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -14,6 +14,8 @@ export async function GET (request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
+  console.log(requestUrl.origin)
+
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(new URL('/', requestUrl.origin))
 }
