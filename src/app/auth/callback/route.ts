252d208180156c89d2d593556ea27776c9855ac1
Reli <naitvenefy@gmail.com>
2024-03-26 14:18:56 -0600

    add route on get redirect

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -14,8 +14,8 @@ export async function GET (request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  console.log(requestUrl.origin)
-
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(new URL('/', requestUrl.origin))
+  return NextResponse.redirect(
+    new URL('https://agorath.vercel.app/', requestUrl.origin)
+  )
 }
