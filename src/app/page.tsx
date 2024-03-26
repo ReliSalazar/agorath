00d95e0585facc365c4fb295558d353902d3afcd
Reli <naitvenefy@gmail.com>
2024-03-25 20:54:04 -0600

    initialize post card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { AuthButtonServer } from '@/app/components/auth-button-server'
+import PostCard from './components/post-card'
 
 export default async function Home () {
   const supabase = createServerComponentClient({ cookies })
-  const { data: { session } } = await supabase.auth.getSession()
+  const {
+    data: { session }
+  } = await supabase.auth.getSession()
 
   if (session === null) {
     redirect('/login')
@@ -13,13 +16,30 @@ export default async function Home () {
 
   const { data: posts } = await supabase
     .from('posts')
-    .select('*, users(name, user_name, avatar_url)')
+    .select('*, user:users(name, user_name, avatar_url)')
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AuthButtonServer />
 
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
+      {posts?.map((post) => {
+        const { id, content, user } = post
+        const {
+          name: userFullName,
+          user_name: userName,
+          avatar_url: avatarUrl
+        } = user
+
+        return (
+          <PostCard
+            avatarUrl={avatarUrl}
+            content={content}
+            key={id}
+            userFullName={userFullName}
+            userName={userName}
+          />
+        )
+      })}
     </main>
   )
 }
